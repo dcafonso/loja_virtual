b290fb8ee4ca8d763b08f8211ce86c68496e237d
Daniel Carlos Afonso <danielcarlosafonso@gmail.com>
2020-03-03 20:52:16 -0300

    Definição dos contratos(interfaces)

---
+++ QuickBuy/QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using QuickBuy.Dominio.Contratos;
+using System.Collections.Generic;
+
+namespace QuickBuy.Repositorio.Repositorios
+{
+    public class BaseRepositorio<TEntity> : IBaseRepositorio<TEntity> where TEntity : class
+    {
+        public BaseRepositorio()
+        {
+
+        }
+
+        public void Add(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> FindAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TEntity FindById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
