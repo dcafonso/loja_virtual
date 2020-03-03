54dd0de7f1e5cb7488145683b12f1306e3e4003a
Daniel Carlos Afonso <danielcarlosafonso@gmail.com>
2020-03-03 20:08:42 -0300

    Ajustes nas entidades e implementação de validações

--- QuickBuy/QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy/QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,14 +1,21 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Endereco { get; set; }
+        public int NroEndereco { get; set; }
         public int FormaPagtoId { get; set; }
         public FormaPagto FormaPagto { get; set; }
 
@@ -17,5 +24,15 @@ namespace QuickBuy.Dominio.Entidades
         /// ou muitos itens de pedido.
         /// </summary>
         public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagemValidacao();
+
+            if (ItensPedido.Any())
+            {
+                MensagemValidacao.Add("O Pedido não pode ficar sem item de pedido!");
+            }
+        }
     }
 }
--- QuickBuy/QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy/QuickBuy.Dominio/Entidades/Usuario.cs
@@ -3,22 +3,37 @@ using System.Collections.Generic;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Usuario
+    public class Usuario : Entidade
     {
         public int Id { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
-        public DateTime DataPrevisaoEntrega { get; set; }
-        public string Estado { get; set; }
-        public string Cidade { get; set; }
-        public string Endereco { get; set; }
-        public int NroEndereco { get; set; }
 
         /// <summary>
         /// Usuário poder ter nenhum ou vários pedidos.
         /// </summary>
         public ICollection<Pedido> Pedidos { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagemValidacao();
+
+            if (string.IsNullOrEmpty(Email))
+            {
+                MensagemValidacao.Add("Usuário deve ser preenchido!");
+            }
+
+            if (string.IsNullOrEmpty(Senha))
+            {
+                MensagemValidacao.Add("Senha deve estar preenchida!");
+            }
+
+            if (string.IsNullOrEmpty(Nome))
+            {
+                MensagemValidacao.Add("Nome deve estar preenchido!");
+            }
+        }
     }
 }
